0ce9957e39c09e0898a5bfc7106c3de82ac14b30
Abdul-Rahman-Ammourah <abdulrhmanammourah@gmail.com>
2024-07-28 18:27:05 +0300

    backend Tasks

---
+++ api/Controllers/BirthDay.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/BirthDay")]
+    public class BirthDay
+    {
+        [HttpGet]
+        public string Get([FromQuery] string name = "", [FromQuery] int year = 0, [FromQuery] int month = 0, [FromQuery] int day = 0){
+
+            if (string.IsNullOrEmpty(name))
+            {
+                if (year == 0 || month == 0 || day == 0){
+                    return "Hello anonymous "+" I can not calculate your age without knowing your birthdate!";
+                }
+                else{
+                    
+                    return "Hello anonymous "+" your age is " + (DateTime.Now.Year - year);
+                }
+            }
+            else
+            {
+                if( year == 0 || month == 0 || day == 0){
+                    return "Hello " + name + " "+" I can not calculate your age without knowing your birthdate!";
+                }
+                else{
+                    return "Hello " + name + " "+" your age is " + (DateTime.Now.Year - year);
+                }
+            }
+        }
+        
+    }
+}
---
+++ api/Controllers/Greater.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/Greater")]
+    public class Greater : ControllerBase
+    {
+        [HttpGet]
+        public string Get([FromQuery] string name = "")
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello anonymous";
+            }
+            else
+            {
+                return "Hello " + name;
+            }
+        }
+    }
+}
